e6d86a2bf4ec5d215952705bcf50197e8c81b0c7
Musa Jibril <musjib999@gmail.com>
2020-11-16 11:11:50 +0100

    upload and download

--- server.js
+++ server.js
@@ -1,48 +1,38 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const multer  = require('multer');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var fs = require('fs');
 var path = require('path');
+var upload = multer({ dest: 'uploads/' });
 
+app.post('/convert', upload.single("file"), (req, res) => {
 
-// app.post('/convert', upload.single("file"), function (req, res) {
-//     console.log("Received file " + req.file.originalname);
-//     var file = fs.createReadStream(req.file.path);
-//     var dest = fs.createWriteStream('uploads/' + req.file.originalname);
-//     file.pipe(dest);
-
-
-//     file.on('end', function () {
-//         fs.unlinkSync(req.file.path);
-//         let convertedFile = Date.now() + 'download.pdf';
-//         libreofficeConvert.convert(file, '.pdf', undefined, (err, done) => {
-//             if (err) {
-//                 fs.unlinkSync(req.file.path);
-//                 console.log(`Error converting file: ${err}`);
-//             }
-//             let convert = fs.writeFileSync(convertedFile, done);
-//             // res.json(convert);
-//             console.log(done);
-//         });
-//         res.json({ status: 'success', message: 'OK: received ' + req.file.originalname });
-//     });
-
-//     file.on('error', function (err) { res.json('Something went wrong!', err); });
-
-// });
-
-
-app.post('/convert', (req, res) => {
-    var filePath = path.join(__dirname, 'file.pdf');
-    var stat = fs.statSync(filePath);
-
-    res.writeHead(200, {
-        'Content-Type': 'file/pdf',
-        'Content-Length': stat.size
+    console.log("Received file " + req.file.originalname);
+    var src = fs.createReadStream(req.file.path);
+    var dest = fs.createWriteStream('uploads/' + req.file.originalname);
+    src.pipe(dest);
+    src.on('end', function() {
+    	fs.unlinkSync(req.file.path);        
+        //sending file as a response
+        var filePath = path.join(__dirname, 'file.pdf');
+        var stat = fs.statSync(filePath);
+    
+        res.writeHead(200, {
+            'Content-Type': 'file/pdf',
+            'Content-Length': stat.size
+        });
+    
+        var readStream = fs.createReadStream(filePath);
+        var str = readStream.pipe(res);
+        console.log(str);
     });
-
-    var readStream = fs.createReadStream(filePath);
-    readStream.pipe(res);
+    src.on('error', function(err) { res.json('Something went wrong!', err); });
+   
 });
 
 
